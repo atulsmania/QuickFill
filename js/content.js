d8c5a20eace004a68a034b826af71287ba5154e2
atulsmania <atul14ac@gmail.com>
2023-05-03 05:55:54 +0530

    mutation observer to add event listener on focus

--- js/content.js
+++ js/content.js
@@ -1,49 +1,81 @@
-(function () {
-  "use strict";
-  const localStorageIdentifier = "ff-key";
-  updateStorage();
+("use strict");
 
-  const elements = document.querySelectorAll(
-    "input[type=text]",
-    "input[type=search]",
-    "input[type=url]",
-    "input[type=checkbox]"
-  );
-  elements.forEach((element) => {
-    element.addEventListener("input", function setInputValue(e) {
-      chrome.storage.local.get([localStorageIdentifier], function (data) {
-        const val = getShortcutValue(
-          e.target.value,
-          data[localStorageIdentifier]
-        );
-        if (!val) return;
-        e.preventDefault();
+const localStorageIdentifier = "ff-key";
+chrome.storage.local.get([localStorageIdentifier], function (e) {
+  if (e[localStorageIdentifier]) return;
+  chrome.storage.local.set({ [localStorageIdentifier]: [] });
+});
 
-        const event = new Event("input", { bubbles: true });
-        e.target.value = val;
-        e.target.dispatchEvent(event);
-      });
-    });
-  });
+let lastFocused;
+const observer = new MutationObserver(() => {
+  const focusedElement = document.activeElement;
+  console.log(focusedElement);
+  if (lastFocused === focusedElement) return;
+  lastFocused = focusedElement;
 
-  function updateStorage() {
-    chrome.storage.local.get([localStorageIdentifier], function (e) {
-      if (e[localStorageIdentifier]) return;
-      chrome.storage.local.set({ [localStorageIdentifier]: [] });
-    });
-  }
-
-  function getShortcutValue(value, shortcuts) {
-    let val = "";
-    if (new RegExp(/^@/).test(value)) {
-      shortcuts.find((shortcut) => {
-        if (shortcut.key === value.replace("@", "")) {
-          val = shortcut.value;
-          return true;
-        }
-        return false;
-      });
+  if (
+    focusedElement instanceof HTMLInputElement ||
+    focusedElement instanceof HTMLTextAreaElement
+  ) {
+    if (
+      !focusedElement.__eventListeners ||
+      !focusedElement.__eventListeners["input"] ||
+      !focusedElement.__eventListeners["input"].includes(setInputValue)
+    ) {
+      addEventListenerOnce(focusedElement, "input", setInputValue);
     }
-    return val;
   }
-})();
+});
+
+let updatedValue;
+function setInputValue(e) {
+  if (e.target.value === updatedValue) return;
+
+  chrome.storage.local.get([localStorageIdentifier], function (data) {
+    updatedValue = getUpdatedValue(
+      e.target.value,
+      data[localStorageIdentifier]
+    );
+    if (e.target.value === updatedValue) return;
+    e.preventDefault();
+
+    const event = new Event("input", { bubbles: true });
+    e.target.value = updatedValue;
+    e.target.dispatchEvent(event);
+  });
+}
+
+function getUpdatedValue(inputValue, mappings) {
+  const [key] = inputValue.match(new RegExp(/@\w+$/g));
+  if (key) {
+    mappings.find((shortcut) => {
+      console.log(`@${shortcut.key}`, key);
+      if (`@${shortcut.key}` === key) {
+        inputValue = inputValue.replace(key, shortcut.value);
+        return true;
+      }
+      return false;
+    });
+  }
+  return inputValue;
+}
+
+const addEventListenerOnce = (element, eventName, listener) => {
+  if (!element.__eventListeners) {
+    element.__eventListeners = {};
+  }
+  if (!element.__eventListeners[eventName]) {
+    element.__eventListeners[eventName] = [];
+  }
+  if (!element.__eventListeners[eventName].includes(listener)) {
+    element.addEventListener(eventName, listener);
+    element.__eventListeners[eventName].push(listener);
+  }
+};
+
+observer.observe(document, {
+  subtree: true,
+  childList: true,
+  attributes: true,
+  characterData: true,
+});
